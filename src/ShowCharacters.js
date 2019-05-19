a62e54b2dc0a69646bc2c547f68199996c60c0c2
Inga Pflaumer <inga.pflaumer@gmail.com>
2019-05-19 11:20:15 +1000

    Fix for ShowCharacter

--- src/ShowCharacters.js
+++ src/ShowCharacters.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 const groupSize = 3;
 
-const Character = (props) => console.log('character', props) || (
-  <div className="card text-white bg-primary mb-3" style={{maxWidth: "20rem"}} data-testid="character">
+const Character = (props) => (
+  <div className="card text-white bg-primary mb-3"
+      style={{maxWidth: "20rem"}}
+      data-testid="character">
     <div className="card-body">
       <h4 className="card-title" data-testid="name">{props.character.name}</h4>
         <img
@@ -12,7 +14,14 @@ const Character = (props) => console.log('character', props) || (
           src={props.character.thumbnail.path+"."+props.character.thumbnail.extension}
         />
       <p className="card-text" data-testid="descr">{props.character.description}</p>
-      <button className="btn btn-outline-secondary" data-id={props.character.id} data-testid="deleteButton" onClick={props.removeCharacter}> Delete</button>
+      <button
+        className="btn btn-outline-secondary"
+        data-testid="deleteButton"
+        data-id={props.character.id} 
+        onClick={props.removeCharacter}
+      >
+        Delete
+      </button>
     </div>
   </div>
 );
